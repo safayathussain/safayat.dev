dc70143da3800d0684a693217d7e7b01137761f7
safayathussain <safayatofficial1@gmail.com>
2026-09-05 11:57:38 +0600

    menu color change

--- app/components/TopBar/DotMenuIcon.tsx
+++ app/components/TopBar/DotMenuIcon.tsx
@@ -1,9 +1,9 @@
 export const DotMenuIcon: React.FC = () => {
   return (
     <div className="dot-menu-icon bounce relative flex w-6 justify-between py-1">
-      <span className="absolute top-0.5 left-0 duration-300" />
-      <span className="absolute top-0.5 left-[9px] duration-300 [--delay:0.5s]" />
-      <span className="absolute top-0.5 right-0 duration-300 [--delay:1s]" />
+      <span className="absolute top-0.5 left-0 duration-300 bg-white/80" />
+      <span className="absolute top-0.5 left-[9px] duration-300 [--delay:0.5s] bg-white/80" />
+      <span className="absolute top-0.5 right-0 duration-300 [--delay:1s] bg-white/80" />
     </div>
   )
 }
